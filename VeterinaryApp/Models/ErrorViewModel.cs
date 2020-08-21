91be3329f532b45ec82456cd691c0a1dd97a1756
Greddyy <code.enthusiasm@gmail.com>
2020-08-21 23:15:53 +0300

    first commit

---
+++ VeterinaryApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VeterinaryApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
