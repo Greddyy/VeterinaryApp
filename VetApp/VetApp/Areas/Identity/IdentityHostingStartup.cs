5bea095cfddda1ad34b90d5464a031c6937e495f
Greddyy <code.enthusiasm@gmail.com>
2020-08-22 23:00:21 +0300

    first commit

---
+++ VetApp/VetApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VetApp.Areas.Identity.Data;
+using VetApp.Data;
+
+[assembly: HostingStartup(typeof(VetApp.Areas.Identity.IdentityHostingStartup))]
+namespace VetApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VetAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("VetAppContextConnection")));
+
+                services.AddDefaultIdentity<VetAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<VetAppContext>();
+            });
+        }
+    }
+}
